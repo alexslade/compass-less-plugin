157561fee0f1d236fdee1c4ecf0a1fb9ffeed4db
William Wells <projects@hanwells.me>
2011-01-14 13:13:12 -0600

    Added iPhone4 media query partial.

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -4,6 +4,7 @@ stylesheet "partials/_3.sass"
 stylesheet "partials/_5.sass"
 stylesheet "partials/_8.sass"
 stylesheet "partials/_13.sass"
+stylesheet "partials/_iphone4.scss"
 
 description "Less Framework"
 
